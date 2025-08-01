77bd8c532aa4fe0615404d55a1b7acfd26609925
Santiago <santiagodeandrade4@gmail.com>
2025-08-01 10:41:49 -0400

    Movimiento de variables a gpu

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from torchvision import datasets
+import torch
 from utils.convert_dataset import convert_dataset
+from sklearn.model_selection import train_test_split
+
+
+print(f'Usando dispositivo {torch.cuda.get_device_name(0)}')
 
 train_dataset = datasets.FashionMNIST(
     root='./data',
@@ -16,4 +21,13 @@ test_dataset = datasets.FashionMNIST(
 
 X_train,Y_train = convert_dataset(train_dataset)
 X_test,Y_test = convert_dataset(test_dataset)
+
+X_val, X_test, Y_val, Y_test = train_test_split(X_test, Y_test, test_size=0.5, random_state=42, stratify=Y_test)
+
+X_train = X_train.to('cuda')
+Y_train = Y_train.to('cuda')
+X_val  = X_val.to('cuda')
+Y_val  = Y_val.to('cuda')
+X_test = X_test.to('cuda')
+Y_test = Y_test.to('cuda')
 
